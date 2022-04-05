aaea3fc10a0c5a647b6bf0436bda3520067644be
Xhoni <61250993+Xhoni792@users.noreply.github.com>
2022-04-05 11:45:46 +0200

    Update level3.php

--- SQLi-CTF-master/www/src/level3.php
+++ SQLi-CTF-master/www/src/level3.php
@@ -23,6 +23,7 @@ if (isset($_GET['q'])) {
 <html>
  <head>
   <title>FISA Search</title>
+	 <link rel="stylesheet" href="http://www.goglogo.com/include/goglogo.css" type="text/css" />
  <img src="https://import.cdn.thinkific.com/440725%2Fcustom_site_themes%2Fid%2FwTEtDV5Q4ubz2x79gMiB_FISA%20Logo%20t1.png?width=384&dpr=1" alt="FISA logo">	 
   </head>
 
